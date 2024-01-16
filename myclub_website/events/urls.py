6e7f24f092e65985eec0f9d53afd53c4f7ae9ba7
suraj-2021 <madwilly6@gmail.com>
2024-01-16 19:50:03 +0530

    Day to Progress

--- myclub_website/events/urls.py
+++ myclub_website/events/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from. import views
 
 urlpatterns = [
-    path(' ',views.home, name = 'home'),
+    #path('home/',views.home, name ='home'),
+    path('<int:year>/<str:month>',views.home, name ='home'),
 ]
+
--- myclub_website/events/views.py
+++ myclub_website/events/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
-
-def home(request):
-    return render(request,'home.html',{})
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
+def home(request, year, month):
+    # convert month from name to number
+    month_to_num = {v: k for k,v in enumerate(calendar.month_name)}
+    month_number = month_to_num.get(month.capitalize())
+    cal = HTMLCalendar().formatmonth(year,month_number)
+    now = datetime.now()
+    #Current time
+    time = now.strftime('%I:%M:%S %p')
+    
+    return render(request, 'home.html', {"year": year, "month": month, "month_number": month_number,"cal":cal,"now": now,"time":time})
